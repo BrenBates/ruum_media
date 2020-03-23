08650ad826407dcc38ed06d8444ff121af03e4c6
Bren <brennenbates2859@gmail.com>
2020-03-23 15:29:57 -0500

    initial file creations

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  userImage: { type: String, default: 'https://cdn.images.express.co.uk/img/dynamic/79/590x/secondary/Emilia-Clarke-Game-Of-Thrones-Instagram-pictures-new-dog-puppy-2270102.jpg?r=1579617194786', required: true},
+  date: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
