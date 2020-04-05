f81cdd41beec3d5b115c6aa72ff337373810936c
Bren-lap <brennenbates2859@gmail.com>
2020-04-05 14:10:08 -0500

    Beginnings of a RBAC system
    following along with this article

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -5,15 +5,24 @@ import { useAuth0 } from '../../react-auth0-spa';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, getTokenSilently, getTokenWithPopup, logout } = useAuth0();
 
   return (
     <div>
       {!isAuthenticated && (
+        <>
         <button onClick={() => loginWithRedirect({})}>Log in</button>
+        {/* <button onClick={() => getTokenSilently({})}>Get Token</button> */}
+        </>
       )}
 
-      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+      {isAuthenticated && (
+        <>
+      <button onClick={() => logout()}>Log out</button>
+      <button onClick={() => getTokenSilently({})}>Get Token Silently</button>
+      {/* <button onClick={() => getTokenWithPopup({})}>Get Token With Popup</button> */}
+       </>
+      )}
 
       {isAuthenticated && (
         <span>
---
+++ client/src/rbac-rules.js
@@ -0,0 +1,40 @@
+// Static permissions are those permissions which don't need any data apart from the user role. 
+// Dynamic permissions are permissions which need additional data to determine access. 
+// Static permissions are helpful when, for example, you have to allow write access to writers and admins only. 
+// Dynamic permissions are helpful when, for example, you have only to allow the writer who is the owner of a resource to edit that resource.
+
+
+const rules = {
+    visitor: {
+      static: ["posts:list", "home-page:visit"]
+    },
+    writer: {
+      static: [
+        "posts:list",
+        "posts:create",
+        "users:getSelf",
+        "home-page:visit",
+        "dashboard-page:visit"
+      ],
+      dynamic: {
+        "posts:edit": ({userId, postOwnerId}) => {
+          if (!userId || !postOwnerId) return false;
+          return userId === postOwnerId;
+        }
+      }
+    },
+    admin: {
+      static: [
+        "posts:list",
+        "posts:create",
+        "posts:edit",
+        "posts:delete",
+        "users:get",
+        "users:getSelf",
+        "home-page:visit",
+        "dashboard-page:visit"
+      ]
+    }
+  };
+  
+  export default rules;
