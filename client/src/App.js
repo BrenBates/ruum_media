1b0bee702c1a87c24dfc552bc11e47ce003ed96c
rpupunu <rpupunu@gmail.com>
2020-04-15 16:22:15 -0600

    added a logo page so that we have a URL for our logo and added the color scheme to the master jss file

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import Home from './pages/home/home';
 import Landing from './pages/landing/landing';
 import NoMatch from './pages/noMatch/noMatch';
 import {useAuth0} from './react-auth0-spa';
-import Profile from './pages/profile/profile';
+import Profile from './pages/profile/Profile';
 import history from './utils/history';
+import Logo from './pages/logos/logo';
 
 
 function App(props) {
@@ -23,6 +24,7 @@ function App(props) {
   
           <Switch>
             <Route exact path="/" component={Landing} />
+            <Route exact path="/logo" component={Logo} />
             <Route path="/home" component={Home} />
             <Route path="/profile" component={Profile} /> 
             <Route component={NoMatch} />
